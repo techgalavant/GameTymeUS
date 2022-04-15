d4f175b83feb5c5cee7476514306e69baf6e885f
Mike Fallon <tech.galavant@gmail.com>
2022-04-15 13:21:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python2
+
+from random import *
+
+from pip._vendor.distlib.compat import raw_input
+
+player_score = 0
+computer_score = 0
+
+
+def hangedman(hangman):
+    graphic = [
+        """
+        +-------+
+        |
+        |
+        |
+        |
+        |
+    =================
+    """,
+        """
+        +-------+ 
+        |       |
+        |       O
+        |
+        |
+        |
+    =================
+    """,
+        """
+        +-------+
+        |       |
+        |       O
+        |       |
+        |
+        |
+        |
+    =================
+    """,
+        """
+        +-------+
+        |       |
+        |       O
+        |      -|
+        |
+        |
+        |
+        |
+    =================
+    """,
+        """
+        +-------+
+        |       |
+        |       O
+        |      -|-
+        |
+        |
+    =================
+    """,
+        """
+        +-------+
+        |       |
+        |       O
+        |      -|-
+        |      /
+        |
+    =================
+    """,
+        """
+        +-------+
+        |       |
+        |       O
+        |      -|-
+        |      / L
+        |
+    =================
+    """]
+
+    print(graphic[hangman])
+    return
+
+
+def start():
+    print("Let's play a game of Hangman.")
+    while game():
+        pass
+    scores()
+
+
+def game():
+    dictionary = ["dog", "puppy", "canine", "scooby", "snoopy", "treats"]
+    word = choice(dictionary)
+    word_length = len(word)
+    clue = word_length * ["_"]
+    print(" ".join(clue))
+    print("This word has " + str(word_length) + " letters.")
+    tries = 6
+    letters_tried = ""
+    guesses = 0
+    letters_right = 0
+    letters_wrong = 0
+    global computer_score, player_score
+
+    while (letters_wrong != tries) and ("".join(clue) != word):
+        letter = guess_letter()
+        if len(letter) == 1 and letter.isalpha():
+            if letters_tried.find(letter) != -1:
+                print("You've already picked ", letter)
+            else:
+                letters_tried = letters_tried + letter
+                first_index = word.find(letter)
+                if first_index == -1:
+                    letters_wrong += 1
+                    print("Sorry,", letter, "isn't found in this word.")
+                else:
+                    print("Congratulations!", letter, "is found in this word.")
+                    for i in range(word_length):
+                        if letter == word[i]:
+                            clue[i] = letter
+        else:
+            print("Choose another letter.")
+
+        hangedman(letters_wrong)
+        print(" ".join(clue))
+        print("Guesses: ", letters_tried)
+
+        if letters_wrong == tries:
+            print("Game Over.")
+            print("The word was", word + ".")
+            computer_score += 1
+            break
+        if "".join(clue) == word:
+            print("You Win!")
+            print("The word was", word + ".")
+            player_score += 1
+            break
+    return play_again()
+
+
+def guess_letter():
+    print
+    letter = raw_input("Guess a letter: ")
+    letter.strip()
+    letter.lower()
+    print
+    return letter
+
+
+def play_again():
+    answer = raw_input("Would you like to play again? y/n: ")
+    if answer in ("y", "Y", "yes", "Yes", "Sure", "sure", "OK", "ok", "okay"):
+        return answer
+    else:
+        print("Thank you for playing! See you next time!")
+
+
+def scores():
+    global player_score, computer_score
+    print("HIGH SCORES")
+    print("Player: ", player_score)
+    print("Computer: ", computer_score)
+
+
+if __name__ == '__main__':
+    start()
